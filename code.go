9929bcd6a1c4023242f587949ddee93ce1ce33a3
venomuz <venom.uzz@mail.ru>
2022-05-12 23:03:29 +0500

    code.go

--- code.go
+++ code.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"io"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-	read := bufio.NewReader(os.Stdin)
-	readed, _ := read.ReadString('\n')
-	sp := strings.Split(readed, string(','))
-	
+	rd := bufio.NewReader(os.Stdin)
+	timeStr, err := rd.ReadString('\n')
+	if err != nil && err != io.EOF {
+		panic(err)
+	}
+	timeStr = strings.TrimSuffix(timeStr, "\n")
+	mass := strings.Split(timeStr, string(','))
+	tim, err := time.Parse("02.01.2006 15:04:05", mass[0])
+	if err != nil {
+		panic(err)
+	}
+	tim1, err := time.Parse("02.01.2006 15:04:05", mass[1])
+	if err != nil {
+		panic(err)
+	}
+	asd := fmt.Sprintln(tim.Sub(tim1))
+	asd = strings.TrimSuffix(asd, "\x0a")
+	dur, err := time.ParseDuration(asd)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(dur)
 }
