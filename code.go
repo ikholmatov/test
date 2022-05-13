1a18722d5041ed86ab8d1da464207cf7a4d74da9
venomuz <venom.uzz@mail.ru>
2022-05-13 08:47:11 +0500

    ii

--- code.go
+++ code.go
@@ -17,19 +17,24 @@ func main() {
 	}
 	timeStr = strings.TrimSuffix(timeStr, "\n")
 	mass := strings.Split(timeStr, string(','))
-	tim, err := time.Parse("02.01.2006 15:04:05", mass[0])
-	if err != nil {
-		panic(err)
-	}
-	tim1, err := time.Parse("02.01.2006 15:04:05", mass[1])
-	if err != nil {
-		panic(err)
-	}
-	asd := fmt.Sprintln(tim.Sub(tim1))
-	asd = strings.TrimSuffix(asd, "\x0a")
-	dur, err := time.ParseDuration(asd)
+	//tim, err := time.Parse("02.01.2006 15:04:05", mass[0])
+	//if err != nil {
+	//	panic(err)
+	//}
+	//tim1, err := time.Parse("02.01.2006 15:04:05", mass[1])
+	//if err != nil {
+	//	panic(err)
+	//}
+	//asd := fmt.Sprintln(tim.Sub(tim1))
+	//asd = strings.TrimSuffix(asd, "\x0a")
+	dur, err := time.ParseDuration(mass[0])
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(dur)
+	dur1, err := time.ParseDuration(mass[1])
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(dur1)
 }
