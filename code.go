892e29b1d8524ae3cb29071c28bb89a37f57cb8f
venomuz <venom.uzz@mail.ru>
2022-05-15 19:43:37 +0500

    Test3

--- code.go
+++ code.go
@@ -1,40 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"io"
-	"os"
-	"strings"
-	"time"
-)
+import "fmt"
 
 func main() {
-	rd := bufio.NewReader(os.Stdin)
-	timeStr, err := rd.ReadString('\n')
-	if err != nil && err != io.EOF {
-		panic(err)
-	}
-	timeStr = strings.TrimSuffix(timeStr, "\n")
-	mass := strings.Split(timeStr, string(','))
-	//tim, err := time.Parse("02.01.2006 15:04:05", mass[0])
-	//if err != nil {
-	//	panic(err)
-	//}
-	//tim1, err := time.Parse("02.01.2006 15:04:05", mass[1])
-	//if err != nil {
-	//	panic(err)
-	//}
-	//asd := fmt.Sprintln(tim.Sub(tim1))
-	//asd = strings.TrimSuffix(asd, "\x0a")
-	dur, err := time.ParseDuration(mass[0])
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(dur)
-	dur1, err := time.ParseDuration(mass[1])
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(dur1)
+	fmt.Println("Hello world !")
 }
---
+++ test3.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+type data interface {
+	Update()
+	Get(string) ([]info, error)
+	Delete()
+}
+type info struct {
+	id                int64
+	jobtitle          string
+	emailaddress      string
+	firstnamelastname string
+	phonenumber       string
+	country           string
+}
+
+func (info) Get(db string) ([]info, error) {
+
+	ss, err := sql.Open("postgres", db)
+	if err != nil {
+		log.Panicf("%s\n%s", err, "Error while opening db")
+	}
+	defer ss.Close()
+	myqueary := `SELECT firstnamelastname,country,emailaddress,jobtitle FROM mockdata ORDER BY firstnamelastname ASC; `
+	rows, err := ss.Query(myqueary)
+	if err != nil {
+		log.Panicf("%s\n%s", err, "Error while get info from db")
+	}
+	defer func(rows *sql.Rows) {
+		if err := rows.Close(); err != nil {
+			log.Panicf("%s\n%s", err, "Error while closing rows from db")
+		}
+	}(rows)
+	var inf []info
+	for rows.Next() {
+		get := info{}
+		err := rows.Scan(&get.firstnamelastname, &get.country, &get.emailaddress, &get.jobtitle)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		inf = append(inf, get)
+	}
+	return inf, err
+}
+func main() {
+	mybase := "user=venom password=112233 dbname=venom sslmode=disable"
+	var w info
+	get, err := w.Get(mybase)
+	if err != nil {
+		panic(err)
+	}
+	for _, v := range get {
+		fmt.Println(v.firstnamelastname)
+	}
+
+}
