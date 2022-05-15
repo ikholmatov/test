4b8bf436629461a477c3b2594d78c116a0b2a1a5
venomuz <venom.uzz@mail.ru>
2022-05-15 22:58:27 +0500

    done

--- test3.go
+++ test3.go
@@ -7,6 +7,31 @@ import (
 	"log"
 )
 
+func main() {
+	//mybase := "user=venom password=112233 dbname=venom sslmode=disable"
+	//var w info
+	//Code for use get
+	//get, err := w.Get(33, mybase)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//for _, v := range get {
+	//	fmt.Println(v.id, v.firstnamelastname, v.jobtitle, v.emailaddress, v.country, v.phonenumber)
+	//}
+	//Code for use delete
+	//s, err := w.Delete(99, mybase)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(s)
+	//Code for use Upadta
+	//update, err := w.Update(33, "Devops Engineer", mybase)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(update)
+}
+
 type data interface {
 	Update()
 	Get(string) ([]info, error)
@@ -21,15 +46,15 @@ type info struct {
 	country           string
 }
 
-func (info) Get(db string) ([]info, error) {
+func (info) Get(id int, db string) ([]info, error) {
 
 	ss, err := sql.Open("postgres", db)
 	if err != nil {
 		log.Panicf("%s\n%s", err, "Error while opening db")
 	}
 	defer ss.Close()
-	myqueary := `SELECT firstnamelastname,country,emailaddress,jobtitle FROM mockdata ORDER BY firstnamelastname ASC; `
-	rows, err := ss.Query(myqueary)
+	myqueary := `SELECT phonenumber,id,firstnamelastname,country,emailaddress,jobtitle FROM mockdata WHERE id = $1; `
+	rows, err := ss.Query(myqueary, id)
 	if err != nil {
 		log.Panicf("%s\n%s", err, "Error while get info from db")
 	}
@@ -41,7 +66,7 @@ func (info) Get(db string) ([]info, error) {
 	var inf []info
 	for rows.Next() {
 		get := info{}
-		err := rows.Scan(&get.firstnamelastname, &get.country, &get.emailaddress, &get.jobtitle)
+		err := rows.Scan(&get.phonenumber, &get.id, &get.firstnamelastname, &get.country, &get.emailaddress, &get.jobtitle)
 		if err != nil {
 			fmt.Println(err)
 			continue
@@ -51,23 +76,29 @@ func (info) Get(db string) ([]info, error) {
 	return inf, err
 }
 
-func main() {
-	mybase := "user=venom password=112233 dbname=venom sslmode=disable"
-	var w info
-	get, err := w.Get(mybase)
-	if err != nil {
-		panic(err)
-	}
-	for _, v := range get {
-		fmt.Println(v.firstnamelastname)
-	}
-
-}
 func (info) Delete(id int, db string) (string, error) {
 	ss, err := sql.Open("postgres", db)
 	if err != nil {
 		log.Panicf("%s\n%s", err, "Error while opening db")
 	}
 	defer ss.Close()
-	myquery := (`DELETE FROM mockdata where id $1`)
+	myquery := `DELETE FROM mockdata where id = $1`
+	_, err = ss.Exec(myquery, id)
+	if err != nil {
+		log.Panicf("%s\n%s", err, "Error while deleting info from table")
+	}
+	return "Ok", err
+}
+func (info) Update(id int, nam string, db string) (string, error) {
+	ss, err := sql.Open("postgres", db)
+	if err != nil {
+		log.Panicf("%s\n%s", err, "Error while opening db")
+	}
+	defer ss.Close()
+	myquery := `UPDATE mockdata SET jobtitle = $2 WHERE id = $1`
+	_, err = ss.Exec(myquery, id, nam)
+	if err != nil {
+		panic(err)
+	}
+	return "Ok", err
 }
